b8df86e99bdbf9d6c5cc8e7d1fe5e7804cf4044e
dirage1 <dirage4635@gmail.com>
2025-05-30 17:12:10 +0900

    SCSS에서 styledcomponents로 수정 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,42 @@
-import "./App.scss";
 import { contents } from "./assets/data/data";
 import Content from "./components/Content";
 import Header from "./components/Header";
+import styled from "styled-components"
+import { flexMixin } from "./components/styled";
+import { createGlobalStyle } from "styled-components";
+
+const SectionStyled = styled.section`
+  ${flexMixin({
+    justify: 'center', 
+    align: 'center', 
+    wrap: 'wrap' , 
+    gap: '20px'})};
+  padding: 20px 40px;
+`
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    color: white;
+  }
+
+  body {
+    background-color: #121212;
+  }
+`
 
 function App() {
   return (
     <main>
+      <GlobalStyle/>
       <Header />
-      <section>
+      <SectionStyled>
         {contents.map((el) => (
           <Content key={el.id} content={el} />
         ))}
-      </section>
+      </SectionStyled>
     </main>
   );
 }
--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,10 +1,39 @@
+import { flexMixin, fontMixin, tagcolor, gray} from "./styled";
+import styled from "styled-components"
+
+export const ContentStyled = styled.div`
+  ${flexMixin({direction: 'column', align: 'flex-start', gap: '5px'})};
+  img {
+    width: 300px;
+    border-radius: 10px;
+    margin-bottom: 3px;
+    min-height: 225px;
+  }
+  span {
+    ${fontMixin({size: '12px'})};
+    color: ${tagcolor};
+    border: 1px solid ${tagcolor};
+    padding: 4px 5px;
+    border-radius: 3px;
+  }
+  div {
+    ${fontMixin({size: '18px', weight: '600'})};
+  }
+  p {
+    ${fontMixin({size: '12px'})};
+    color: ${gray};
+  }
+`
+
+
+
 export default function Content({ content }) {
   return (
-    <div className="content-container">
+    <ContentStyled>
       <img src={content.img} alt={content.title} />
       <span>모집중</span>
       <div>{content.title}</div>
       <p>{content.subtitle}</p>
-    </div>
+    </ContentStyled>
   );
 }
